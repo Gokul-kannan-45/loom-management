1bd007e86491736160c08f58e6b0496af13d3658
Gokul-kannan-45 <gkmech45@gmail.com>
2024-02-24 14:46:12 +0530

    db connection issue fixed

--- src/service/warp_service.py
+++ src/service/warp_service.py
@@ -2,13 +2,13 @@ import datetime
 
 class WarpService():
   def __init__(self,app,userId=None):
-    self.warpDb = app.database['loom'] 
+    self.warpDb = app.database['warp'] 
     pass
 
   def getWarp(self,userId):
     print("**************")
     print(self.warpDb)
-    warpObj = self.warpDb.find({"name":"Gokul"},{"_id":0})
+    warpObj = self.warpDb.find_one({"id":1001},{"_id":0})
     print(warpObj)
     print("inside getWarp method")
     return warpObj
